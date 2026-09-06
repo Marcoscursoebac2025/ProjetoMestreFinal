e28e9ee5ca5fac119129e053e3925ef71577bfe8
Marcos Bello <marcosbello2555@gmail.com>
2026-09-06 13:09:10 -0300

    update package.json

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
  
-  "scripts": {
-    "start": "http-server ./public -p 3000 -e html",
-     baseUrl: "http://127.0.0.1:3000"
-  }
+     baseUrl: "http://localhost:3000/"
+
 }
- 
 })
