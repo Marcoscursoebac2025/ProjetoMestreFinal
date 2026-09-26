49318ca52b56668933c006a91fcb9651b06fb113
Marcos Bello <marcosbello2555@gmail.com>
2026-09-26 18:27:25 -0300

    alteraçao package.json

--- cypress/server.js
+++ cypress/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const app = require(path.resolve(__dirname, "../../hub-de-leitura-integrado/src/server"));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`🚀 Servidor levantado en http://localhost:${port}`);
